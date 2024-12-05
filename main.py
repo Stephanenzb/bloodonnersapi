dd6b25c7fb9d80d8382d4ce1dfda076b9acc0386
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2024-12-05 13:43:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, donors, admin
+
+
 app = FastAPI()
+
 # Configuration de CORS
 app.add_middleware(
     CORSMiddleware,
-@@ -10,10 +23,200 @@
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 # Inclusion des routes
 app.include_router(auth.router, prefix="/api", tags=["auth"])
 app.include_router(donors.router, prefix="/api", tags=["donors"])
